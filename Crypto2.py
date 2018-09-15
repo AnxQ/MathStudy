de913080108912574fb2d3a5668b74675617fef2
AnxQ <xiaoqi990112@hotmail.com>
2018-09-15 10:57:08 +0800

    Fix: Missing some items in Pinyin dict. Fix: gcd(a,b) may result in negative. Fix: Add words in English dict. Add: CUMCM2018 algorithms

--- Crypto2.py
+++ Crypto2.py
@@ -4,16 +4,17 @@ import xlrd
 ALPHABET_DICT: dict = dict(zip([chr(i) for i in range(65, 91)], [i % 26 for i in range(1, 27)]))
 NUMERIC_DICT: dict = dict(zip(ALPHABET_DICT.values(), ALPHABET_DICT.keys()))
 
-workbook = xlrd.open_workbook(r'./essential_words_cn.xls')
+workbook = xlrd.open_workbook(r'./essential_words_en.xls')
 word_sheet = workbook.sheet_by_index(1)
 NORMAL_WORD = list(map(lambda x: x.upper(), word_sheet.col_values(0)))
 NORMAL_WORD.sort(key=lambda x: len(x), reverse=True)
+
 
 def gcd(a, b):
     (a, b) = (b, a) if a < b else (a, b)
     while b:
         a, b = b, a % b
-    return a
+    return abs(a)
 
 
 def inv_m26(m: np.matrix):
@@ -30,20 +31,17 @@ def inv_m26(m: np.matrix):
 def encrypt(dcrypted_str: str, key_matrix: np.matrix):
     if dcrypted_str.__len__() % 2:
         dcrypted_str = dcrypted_str + dcrypted_str[-1]
-    ori_m = np.matrix(list(map(lambda x: ALPHABET_DICT[x], dcrypted_str))).reshape(-1, 2)
-    enc_m = np.dot(ori_m, key_matrix).reshape(1, -1)
+    ori_m = np.matrix(list(map(lambda x: ALPHABET_DICT[x], dcrypted_str))).reshape(-1, 2).T
+    enc_m = np.dot(key_matrix, ori_m).T.reshape(1, -1)
     return ''.join(map(lambda x: NUMERIC_DICT[x % 26], enc_m.tolist()[0]))
 
 
 def decrypt(ecrypted_str: str, key_matrix: np.matrix) -> str:
     if ecrypted_str.__len__() % 2:
         ecrypted_str = ecrypted_str + ecrypted_str[-1]
-    ori_m = np.matrix(list(map(lambda x: ALPHABET_DICT[x], ecrypted_str))).reshape(-1, 2)
-    dec_m = np.dot(ori_m, inv_m26(key_matrix)).reshape(1, -1)
-    try:
-        return ''.join(map(lambda x: NUMERIC_DICT[round(x % 26) if abs(x - round(x)) < 1e-5 else x], dec_m.tolist()[0]))
-    except KeyError:
-        return None
+    ori_m = np.matrix(list(map(lambda x: ALPHABET_DICT[x], ecrypted_str))).reshape(-1, 2).T
+    dec_m = np.dot(inv_m26(key_matrix), ori_m).T.reshape(1, -1)
+    return ''.join(map(lambda x: NUMERIC_DICT[round(x) % 26], dec_m.tolist()[0]))
 
 
 def crack(ecrypted_str: str, max_hope: int):
@@ -52,17 +50,16 @@ def crack(ecrypted_str: str, max_hope: int):
         for b in range(0, max_hope):
             for c in range(0, max_hope):
                 for d in range(0, max_hope):
-                    if not abs(a * d - b * c) < 1e-5:
-                        key = np.matrix([[a, b], [c, d]])
-                        if inv_m26(key) is None:
-                            continue
-                        res = decrypt(ecrypted_str, key)
-                        if res is not None:
-                            result.append([
-                                # sum(map(lambda x: res.count(x), NORMAL_WORD)),
-                                judge(res),
-                                key,
-                                res])
+                    key = np.matrix([[a, b], [c, d]])
+                    if inv_m26(key) is None:
+                        continue
+                    res = decrypt(ecrypted_str, key)
+                    if res is not None:
+                        result.append([
+                            sum(map(lambda x: res.count(x), NORMAL_WORD)),
+                            # judge(res),
+                            key,
+                            res])
     result.sort(key=lambda x: x[0], reverse=True)
     return result
 
@@ -83,8 +80,41 @@ def judge(string: str) -> int:
     return current
 
 
-if __name__ == '__main__':
-    TEST_STR: str = "EXPROIEMRECAAAWMFYEQASXWAJFIDLCACGDSYPKPOIZKUTSLPGMEZSCARMOYOIONAJYSYQRUDSUTXADLCAAABIZVWMFY"
+def long_near(pair1, pair2):
+    if pair1[1] < pair2[1]:
+        return pair1
+    elif pair1[1] == pair2[1]:
+        return max(pair1, pair2, key=lambda x: len(x[0]))
+    else:
+        return pair2
 
-    res = crack(TEST_STR, 26)
-    print(list(filter(lambda x: x[2][0] == 'N', res)), res.__len__())
+
+def split_text(string):
+    final_list = []
+    while len(string) > 0:
+        current = ['', 10000]
+        flag = True
+        for w in NORMAL_WORD:
+            pos = string.find(w)
+            if not pos == -1:
+                flag = False
+                current = long_near(current, [w, pos])
+        if current[1] == 0:
+            string = string[len(current[0]):]
+            final_list.append(current[0])
+        else:
+            final_list.append(string[:current[1]])
+            string = string[current[1]:]
+    return " ".join(final_list)
+
+
+if __name__ == '__main__':
+    TEST_STR: str = "AOBZKNJUYSWCUQCDHSSYHISHVGABZPLIZFSVDMXMABKYNZTCOPYWNWEEQFSHMQBWKWMQPEOGUQUYMSWPPGKUDIOIKGSEQAUMMQFLKLTNXYIFQVCCYZXUEZCDMDDASEKNRWQYSEZYLXXKKXKLLSPEIGEXQAKBAHEJSRNUAOCAWBLWFAEWFAGOFHUTOPQAWCGUKNVGMQBWKWAOVIGJHQKXTMJHIGQYLXYHRUSEOQJHSRCMBIABUMXLLWN"
+
+    MY_STR = "SHUXUEHAONAN"
+    MY_KEY = np.matrix([[11, 3], [8, 7]])
+    res = decrypt(TEST_STR, MY_KEY)
+    print(res)
+    print(split_text(res))
+    # res = crack(TEST_STR, 26)
+    # print(list(filter(lambda x: x[0] > 8, res)), res.__len__())
--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,10 @@
 import numpy as np
+from functools import reduce
 
 
 def fib_np(n: int):
     """
     使用 Numpy 进行矩阵运算
-    速度堪比 Matlab 运算速度
     缺陷是：由于 Numpy 的限制，超过第1476项时将会导致矩阵中出现Inf，无法继续迭代
     Normal E.g.
     >>> fib_np(1000)
@@ -13,25 +13,51 @@ def fib_np(n: int):
     :param n: Range from 1 to 1476
     :return: the Fibonacci number
     """
-    R = np.ones([2, n])
+    R = np.ones([2, 1])
     T = np.matrix([[1, 1], [0, 1]])
-    for i in range(1, n):
-        R[:, i] = np.dot(np.flipud(R[:, i - 1]), T)
-    return int(R[1, n - 2]) if n > 1 else 1
+    for i in range(1, n-1):
+        R[:, 0] = np.dot(np.flipud(R[:, 0]), T)
+    return int(R[1, 0]) if n > 1 else 1
 
 
-def fib_recursion(n):
+def fib_np_2(n: int):
     """
-    低效率警告.
-    这是一种极其懒散的递归写法，甚至你输入100它都会卡住
+    比上面的fib_np更优雅的写法
     :param n:
     :return:
+    """
+    R = np.ones([2, 1])
+    T = np.matrix([[1, 1], [0, 1]])
+    return reduce(lambda x, y: np.dot(np.flipud(x), y),
+                  [R] + [T for i in range(n - 1)])[1]
+
+def fib_recursion(n: int):
+    """
+    低效率警告.
+    这是一种极其懒散的树状递归，在没有迭代限制的情况下，它会迅速榨干你的系统资源，
+    因此甚至你输入100它都会卡住
+    :param n:
+    :return: the Fibonacci number
     """
     return 1 if n <= 2 else fib_recursion(n - 1) + fib_recursion(n - 2)
 
 
-def fib_tail_recursion(n):
+def fib_tail_recursion(n: int):
+    """
+    相比树状递归，尾递归所造成的空间复杂度更小，运算速度相对较快
+    :param n:
+    :return: the Fibonacci number
+    """
+
+    def fib_tail_iter(n, x, y):
+        return x if n == 0 else fib_tail_iter(n - 1, y, x + y)
+
+    return fib_tail_iter(n, 0, 1)
+
+
+def fib_iterate(n: int):
     pass
 
+
 if __name__ == "__main__":
-    print(fib_recursion(100))
+    print(fib_np_2(1476))
